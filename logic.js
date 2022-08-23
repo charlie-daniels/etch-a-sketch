8956deef62ed41f79bd80e728fd97cd25981a948
charlie-daniels <charlied030401@gmail.com>
2022-08-23 11:27:54 +0100

    add draw event

--- logic.js
+++ logic.js
@@ -1,13 +1,20 @@
 function createTiles(count) {
     let tiles = [];
     const container = document.querySelector('.container-tile');
+
     for (let i = 0; i < count; i++) {
         let tile = document.createElement('span');
-        tile.classList.add('tile', 'blank');
+        tile.classList.add('tile');
         tile.style.cssText = `width:30px;height:30px;outline:.5px solid black;`;
+        tile.addEventListener('mouseover', (e) => {
+            e.target.style.background = 'black';
+        });
         container.appendChild(tile);
         tiles.push(tile);
     }
+    return tiles;
 }
 
-createTiles(256);
+tiles = createTiles(256);
+ 
+
